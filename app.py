9adf1cba7fd821c2cc85484bb126cf6071910a8a
sofiamb31 <sofiamadrigal2303@gmail.com>
2025-05-01 12:27:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 import base64
 
 st.title("Conversión de Texto a Audio")
-image = Image.open('gato_raton.png')
+image = Image.open('olivia_rodrigo.jpeg')
 st.image(image, width=350)
 with st.sidebar:
     st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
